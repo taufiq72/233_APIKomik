0b00d11c3f39ef289145fa35b155d8aa8e1587de
taufiq72 <taufiqq1207@gmail.com>
2026-07-29 10:12:09 +0700

    menambahkan function getGenreById

--- controller/genrecontroller.js
+++ controller/genrecontroller.js
@@ -9,3 +9,17 @@ async function getAllGenre(req, res) {
         res.status(500).json({ error: 'Failed to fetch genre' });
     }
 }
+
+async function getGenreById(req, res) {
+    const { id } = req.params;
+    try {
+        const genre = await db.Genre.findByPk(id);
+        if (!genre) {
+            return res.status(404).json({ error: 'Genre not found' });
+        }
+        res.status(200).json(genre);
+    } catch (err) {
+        console.error('Error fetching genre by ID:', err.message);
+        res.status(500).json({ error: 'Failed to fetch genre by ID' });
+    }
+}
