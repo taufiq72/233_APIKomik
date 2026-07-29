8e385c8159d6663b1453bb7f3310d889d9a554af
taufiq72 <taufiqq1207@gmail.com>
2026-07-29 10:11:16 +0700

    menambahkan function getallgenre

--- controller/genrecontroller.js
+++ controller/genrecontroller.js
@@ -1,1 +1,11 @@
-const db = require('../models');
+const db = require('../models');const db = require('../models');
+
+async function getAllGenre(req, res) {
+    try {
+        const genre = await db.Genre.findAll();
+        res.status(200).json(genre);
+    } catch (err) {
+        console.error('Error fetching genre:', err.message);
+        res.status(500).json({ error: 'Failed to fetch genre' });
+    }
+}
